d4ed4f2fed2c17f5d8597c479c1f8d10c94c664b
Samuel Näsström <nasstromsamuel@icloud.com>
2022-10-30 17:59:14 +0000

    Add more functionality to test

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,4 @@
+const { expect } = require("@jest/globals");
 const {game} = require("../game")
 
 beforeAll(() => {
@@ -9,7 +10,20 @@ beforeAll(() => {
 });
 
 describe("game object contains correct keys", () => {
+
     test("score key exists", () => {
         expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 })
